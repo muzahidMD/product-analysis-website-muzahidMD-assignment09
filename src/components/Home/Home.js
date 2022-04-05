a44585f9010d60a7ad575c369e9e6489babbcc0e
Muzahidul Islam <mujahidul.core@gmail.com>
2022-04-05 14:02:53 +0600

    added custom hook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,18 +3,23 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className="card-container">
-            <div className='class-container'>
-                <div>
-                    <h1 className='heading'>Your Next Laptop</h1>
-                    <h1 className='heading color-blueviolet' >Your Best Laptop</h1>
-                    <p className='heading-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos, enim vero quas molestias quo laborum non est totam praesentium nesciunt nihil excepturi, dolores adipisci eligendi, accusamus similique! Nostrum, quae!</p>
-                    <button className='header-btn'>Live Demo</button>
+        <div >
+            <section className="card-container">
+                <div className='class-container'>
+                    <div>
+                        <h1 className='heading'>Your Next Laptop</h1>
+                        <h1 className='heading color-blueviolet' >Your Best Laptop</h1>
+                        <p className='heading-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos, enim vero quas molestias quo laborum non est totam praesentium nesciunt nihil excepturi, dolores adipisci eligendi, accusamus similique! Nostrum, quae!</p>
+                        <button className='header-btn'>Live Demo</button>
+                    </div>
                 </div>
-            </div>
-            <div className='img-container'>
-                <img src="https://img.etimg.com/thumb/msid-70450624,width-650,imgsize-273503,,resizemode-4,quality-100/dell-expresssign-is-one-of-the-most-impressing-features-on-the-laptop-.jpg" alt="" />
-            </div>
+                <div className='img-container'>
+                    <img src="https://img.etimg.com/thumb/msid-70450624,width-650,imgsize-273503,,resizemode-4,quality-100/dell-expresssign-is-one-of-the-most-impressing-features-on-the-laptop-.jpg" alt="" />
+                </div>
+            </section>
+            <section>
+
+            </section>
         </div>
     );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
 
 const Review = () => {
+    const [reviews, setReviews] = useReviews();
     return (
         <div>
-            <h2>This is Review</h2>
+            <h2>This is Review:{reviews.length}</h2>
         </div>
     );
 };
