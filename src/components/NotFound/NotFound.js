28a78fcd3d971a2a698fcf0a0977d656c6dae961
Muzahidul Islam <mujahidul.core@gmail.com>
2022-04-05 23:47:15 +0600

    this is last commit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div className='notfound'>
             <h1>404</h1>
+            <p><strong>This page is not found</strong></p>
+            <button><Link to='/'>Back To Home</Link></button>
         </div>
     );
 };
