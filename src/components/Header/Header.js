b5804502f096bbfb45f36de52b74b551b9f0b9b3
Muzahidul Islam <mujahidul.core@gmail.com>
2022-04-05 13:38:27 +0600

    home header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,13 @@ import './header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <Link to="/home">HOME</Link>
-            <Link to="/reviews">REVIEWS</Link>
-            <Link to="/dashboard">DASHBOARD</Link>
-            <Link to="/blog">BLOG</Link>
-            <Link to="/about">ABOUT</Link>
+            <div>
+                <Link to="/home">HOME</Link>
+                <Link to="/reviews">REVIEWS</Link>
+                <Link to="/dashboard">DASHBOARD</Link>
+                <Link to="/blog">BLOG</Link>
+                <Link to="/about">ABOUT</Link>
+            </div>
         </div>
     );
 };
